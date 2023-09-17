6c0196d50b2c3fed4621e00c045ee3010ea315a2
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-17 23:56:48 +0200

    Create script.js

---
+++ GitHub-Users/script.js
@@ -0,0 +1,14 @@
+import { GitHubClient } from "./js/GitHubClient.js";
+
+document.querySelector("button").addEventListener("click", element => {
+    const insideBtnTop = element.clientY - element.target.offsetTop;
+    const insideBtnLeft = element.clientX - element.target.offsetLeft;
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = insideBtnTop + "px";
+    circle.style.left = insideBtnLeft + "px";
+    element.target.appendChild(circle);
+    setTimeout(element.target.style.opacity = "0", 300);
+    setTimeout(() => element.target.remove(), 600);
+    new GitHubClient();
+});
