2a74de6b64701f3a3d984bf9048164d477179aca
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-18 17:34:50 +0200

    Update script.js

--- GitHub-Users/script.js
+++ GitHub-Users/script.js
@@ -1,16 +1,11 @@
+import { ButtonAnimation } from "./js/ButtonAnimation.js";
 import { GitHubClient } from "./js/GitHubClient.js";
 
-document.querySelector("button").addEventListener("click", element => {
-    const insideBtnTop = element.clientY - element.target.offsetTop;
-    const insideBtnLeft = element.clientX - element.target.offsetLeft;
-    const circle = document.createElement("span");
-    circle.classList.add("circle");
-    circle.style.top = insideBtnTop + "px";
-    circle.style.left = insideBtnLeft + "px";
-    element.target.appendChild(circle);
-    setTimeout(element.target.style.opacity = "0", 300);
+document.querySelector("button").addEventListener("click", button => {
+    ButtonAnimation(button);
+    setTimeout(button.target.style.opacity = "0", 300);
     setTimeout(() => {
-        element.target.remove();
+        button.target.remove();
         document.querySelector(".roller").style.display = "block";
     }, 600);
     new GitHubClient();
