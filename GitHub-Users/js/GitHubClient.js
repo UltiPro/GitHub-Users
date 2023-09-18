e6559f06a1d44b0be69e75fc7dc0118155f124b5
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-19 00:43:34 +0200

    Update GitHubClient.js

--- GitHub-Users/js/GitHubClient.js
+++ GitHub-Users/js/GitHubClient.js
@@ -7,63 +7,37 @@ export class GitHubClient {
     constructor() {
         this.#header = document.querySelector("header");
         this.#main = document.querySelector("main");
+        //tutaj
         setTimeout(() => {
-            this.#Init().then(response => this.#PrepareHTMLSuccess(response)).catch(error => this.#PrepareHTMLFail(error));
-        }, 1200);
+            this.#Init().then(response => this.#Success(response)).catch(error => this.#Fail(error));
+        }, 2400);
+        //tutaj
     }
 
-    #SetDefault() {
-        this.#header.childNodes.forEach(node => node.remove());
-        this.#main.style.display = "block";
-    }
-
-    #PrepareHTMLSuccess(data) {
-        this.#header.childNodes.forEach(node => node.remove());
-        const input = document.createElement("input");
-        input.setAttribute("type", "text");
-        this.#header.appendChild(input);
-        document.querySelector("main").appendChild(data);
-    }
-
-    #PrepareHTMLFail(data) {
-        this.#SetDefault();
-        const errorBox = document.createElement("div");
-        errorBox.classList.add("error-box");
-        errorBox.innerHTML = `
-            <div></div>
-            <button>Refresh Page</button>
-        `;
-        errorBox.childNodes[0].textContent = data;
-        errorBox.querySelector("button").addEventListener("click", button => {
-            ButtonAnimation(button);
-            setTimeout(() => location.reload(), 400);
-        });
-        this.#main.append(errorBox);
-    }
-
+    //tutaj
     #Init() {
-        return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) { //xhr response type json
             const xhr = new XMLHttpRequest();
             xhr.getResponseHeader("Content-type", "application/json");
-            xhr.open("GET", "https://api.github.com/usersss", true);
+            xhr.open("GET", "https://api.github.com/users", true);
             xhr.send();
             xhr.onload = function () {
                 if (this.status == 200) {
                     const users = JSON.parse(this.responseText);
                     let htmlCode = document.createElement("main");
                     users.forEach(element => {
-                        htmlCode.appendChild(GitHubClient.BuildUserSquare(element.id, element.login, element.avatar_url, element.html_url));
+                        htmlCode.appendChild(GitHubClient.UserBox(element));
                     });
                     resolve(htmlCode);
                 }
                 else {
                     const text = JSON.parse(this.responseText);
-                    reject("Could not resolve server. Check your connection to the internet.");
+                    reject("Could not connect to server. Check your connection to the internet.");
                 }
             }
 
             xhr.onerror = function () {
-                reject(BuildErrorSquare(this.status));
+                reject("Could not connect to server. Check your connection to the internet.");
             }
 
             xhr.onreadystatechange = function () {
@@ -71,26 +45,55 @@ export class GitHubClient {
             }
         });
     }
+    //tutaj
 
-    static BuildErrorSquare(message) {
-        const html = document.createElement('div');
-        html.classList.add("ErrorBox");
-        html.innerText = message;
-        return html;
+    #PrepareDOM() {
+        this.#header.childNodes.forEach(node => node.remove());
+        this.#main.style.display = "block";
     }
 
-    static BuildUserSquare(id, login, imgUrl, urltoAccount) {
-        const html = document.createElement('div');
-        html.classList.add("userBox");
-        html.innerHTML = `
-            <a href="${urltoAccount}">
-                <img src="${imgUrl}" title="User Image" class="userBox-img" />
-            </a>
-            <div class="userBox-body">
-                Id: ${id}<br/>
-                Login: ${login}<br/>
-            </div>
+    #Success(users) {
+        this.#PrepareDOM();
+        this.#header.classList.add("header-after");
+        const searchInput = document.createElement("input");
+        searchInput.setAttribute("type", "text");
+        searchInput.addEventListener("keyup", input => this.#FindUser(input.target.value));
+        this.#header.appendChild(searchInput);
+        this.#main.appendChild(users);
+    }
+
+    #Fail(info) {
+        this.#PrepareDOM();
+        const errorBox = document.createElement("div");
+        errorBox.classList.add("error-box");
+        errorBox.innerHTML = `
+            <div>${info}</div>
+            <button>Refresh Page</button>
         `;
-        return html;
+        errorBox.querySelector("button").addEventListener("click", button => {
+            ButtonAnimation(button);
+            setTimeout(() => location.reload(), 300);
+        });
+        this.#main.append(errorBox);
     }
+
+    //tutaj
+    #FindUser(find) {
+        console.log(find)
+    }
+
+    static UserBox(userData) {
+        const userBox = document.createElement('div');
+        userBox.classList.add("user-box");
+        userBox.innerHTML = `
+            <a href="${userData.html_url}" target="_blank">
+                <img src="${userData.avatar_url}" alt="Could not load image." class="user-box-img"/>
+                <div class="user-box-body">
+                    data jakaś
+                </div>
+            </a>
+        `;
+        return userBox;
+    }
+    //tutaj element.id, element.login
 }
