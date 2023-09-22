96604c6087d742f19c92c01f10a660c96138d339
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-22 17:57:55 +0200

    Update GitHubClient.js

--- GitHub-Users/js/GitHubClient.js
+++ GitHub-Users/js/GitHubClient.js
@@ -3,20 +3,27 @@ import { ButtonAnimation } from "./ButtonAnimation.js";
 export class GitHubClient {
     static #url = "https://api.github.com/users";
     #lastUserId;
+    //tutaj
     #header;
     #roller;
     #main;
+    #footer;
 
     constructor() {
         this.#lastUserId = 0;
         this.#header = document.querySelector("header");
         this.#roller = this.#header.querySelector(".roller");
         this.#main = document.querySelector("main");
-        //tutaj scroll event
+        this.#footer = document.querySelector("footer");
+        addEventListener("scrollend", () => {
+            if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+                this.#LoadMore();
+            }
+        });
         setTimeout(() => this.#Init(), 2400);
+        //tutaj
     }
 
-    //tutaj
     async #Init() {
         try {
             this.#Success(await this.#GetUsersData(this.#lastUserId));
@@ -25,20 +32,19 @@ export class GitHubClient {
             this.#Fail(error.message);
         }
     }
-    //tutaj
 
-    //tutaj
     #Success(users) {
         this.#lastUserId = users[99].id;
         this.#roller.style.display = "none";
+        this.#footer.appendChild(this.#roller);
         this.#header.classList.add("header-after");
         const searchInput = document.createElement("input");
         searchInput.setAttribute("type", "text");
         searchInput.addEventListener("keyup", input => this.#FindUser(input.target.value));
         this.#header.insertBefore(searchInput, this.#header.firstChild);
-        users.forEach(user => this.#main.appendChild(GitHubClient.UserBox(user)));
+        users.forEach(user => this.#main.appendChild(GitHubClient.#UserBox(user)));
+        this.#roller.style.display = "block";
     }
-    //tutaj
 
     #Fail(info) {
         this.#roller.style.display = "none";
@@ -63,11 +69,9 @@ export class GitHubClient {
             xmlHttpRequest.open("GET", GitHubClient.#url + `?per_page=100&since=${lastUserId}`, true);
 
             xmlHttpRequest.onload = function () {
-                //tutaj
                 if (this.status == 200) resolve(this.response);
                 else if (this.status == 403) reject(new Error("API rate limit exceeded. Please try again later."));
                 else reject(new Error("The application encountered a problem while running. Please try again later."));
-                //tutaj
             };
 
             xmlHttpRequest.onerror = function () {
@@ -78,21 +82,27 @@ export class GitHubClient {
         });
     }
 
-    //tutaj
-    async LoadMore() {
+    async #LoadMore() {
         try {
+            if (this.#roller.parentElement != this.#footer) {
+                this.#footer.appendChild(this.#roller);
+                this.#roller.style.display = "block";
+            }
             const users = await this.#GetUsersData(this.#lastUserId);
+            this.#lastUserId = users[99].id;
+            users.forEach(user => this.#main.appendChild(GitHubClient.#UserBox(user)));
         }
         catch (error) {
-
+            //tutaj
+            console.log(error);
+            //tutaj
         }
     }
-    //tutaj
 
     //tutaj
-    async #FindUser(find) {
+    #FindUser(find) {
         try {
-
+            const users = this.#FindUserRequest(find);
         }
         catch (error) {
 
@@ -100,44 +110,33 @@ export class GitHubClient {
     }
     //tutaj
 
-    //tutaj
-    #FindUserRequest(find) {
-        fetch(GitHubClient.#url + `/${find}`, {
+    async #FindUserRequest(find) {
+        return await fetch(GitHubClient.#url + `/${find}`, {
             method: "GET",
             headers: {
                 "Content-type": "application/json"
             }
         }).then(response => {
-            if (response.status >= 200 && response.status < 300) return response.json();
-            else {
-                return response.json().then(errorData => {
-                    console.log(errorData); // tutaj
-                    throw new Error("The application encountered a problem while running. Please try again later.");
-                });
-            }
-        }).catch(error => {
-            console.log(error.message); // tutaj
+            if (response.status == 200) return response.json();
+            else if (response.status == 403) throw new Error("API rate limit exceeded. Please try again later.");
+            else throw new Error("The application encountered a problem while running. Please try again later.");
+        }).catch(_ => {
             throw new Error("Could not connect to server. Check your connection to the internet.");
         });
     }
-    //tutaj
 
-    //tutaj
-    static UserBox(userData) {
-        const userBox = document.createElement('div');
+    static #UserBox(userData) {
+        const userBox = document.createElement("div");
         userBox.classList.add("user-box");
         userBox.innerHTML = `
             <a href="${userData.html_url}" target="_blank">
                 <img src="${userData.avatar_url}" alt="Could not load image." />
                 <div>
                     <span>Nick:</span> ${userData.login}<br/>
-                    <span>Nick:</span> ${fetch(userData.repos_url).then(response => {
-            console.log(response.json());
-        }).catch(error => "Error")}<br/>
+                    <span>ID:</span> ${userData.id}<br/>
                 </div>
             </a>
         `;
         return userBox;
     }
-    //tutaj
 }
